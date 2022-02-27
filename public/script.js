daf7b64050727689553f598f24e9b0f6d238d143
Glitch (glitch-blank-node) <none>
2022-02-27 04:56:26 +0000

    🙍☺️ Checkpoint ./src/pages/results.hbs:291626/10 ./public/script.js:291626/36

--- public/script.js
+++ public/script.js
@@ -1,3 +1,3 @@
 
-document.getElementById("textTheyTyped").value = "Paste in some text here, then hit 'submit' to extract only the questions. \n\nRefresh the site to do another.";
+document.getElementById("textTheyTyped").value = "Paste in some text here, then hit 'submit' to extract only the questions.";
 
