66f4f517c90741c29adea15aff8609ad9a554cea
Clive Thompson <clive@clivethompson.net>
2025-07-03 18:13:32 -0400

    went back to server.js
    went back to server.js outside of api folder

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const fs = require("fs");
 
 // Load wink-nlp, the natural-language tool that we'll use to identify the sentences inside a piece of text
 const winkNLP = require("wink-nlp");
@@ -14,13 +13,14 @@ const nlp = winkNLP(model);
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
+  // set this to true for detailed logging:
   logger: false,
 });
 
 // Setup our static files
 fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "../public"),
-  prefix: "/",
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
 });
 
 // fastify-formbody lets us parse incoming forms
@@ -31,14 +31,16 @@ fastify.register(require("@fastify/view"), {
   engine: {
     handlebars: require("handlebars"),
   },
-  root: path.join(__dirname, "../src/pages"),
+  root: path.join(__dirname, "src/pages"),
 });
 
 // Our main GET home page route, pulls from src/pages/index.hbs
 fastify.get("/", function (request, reply) {
+  // params is an object we'll pass to our handlebars template
   let params = {
     greeting: "Hello Node!",
   };
+  // request.query.paramName <-- a querystring example
   reply.view("index.hbs", params);
 });
 
@@ -49,6 +51,7 @@ fastify.post("/", async function (request, reply) {
   let params = {
     results: sendBack,
   };
+  //let testResult = nlp.string.sentences(testText);
   reply.view("results.hbs", params);
 });
 
@@ -73,8 +76,22 @@ async function findQuestions(text) {
   return questionList;
 }
 
-// Export the fastify instance for Vercel
-module.exports = async (req, res) => {
-  await fastify.ready();
-  fastify.server.emit('request', req, res);
-};
+// For Vercel serverless deployment
+if (process.env.VERCEL) {
+  module.exports = async (req, res) => {
+    await fastify.ready();
+    fastify.server.emit('request', req, res);
+  };
+} else {
+  // Run the server locally
+  const start = async () => {
+    try {
+      await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' });
+      console.log(`Your app is listening on port ${process.env.PORT || 3000}`);
+    } catch (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  };
+  start();
+}
