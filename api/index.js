4d018b9f727f7256e60e3f24c792ce4d963074de
Clive Thompson <clive@clivethompson.net>
2025-07-03 18:06:00 -0400

    Adding vercel-specific stuff
    Added vercel.json, server.js -> index.js, exported server as module, put index.js in new api folder

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 // Load wink-nlp, the natural-language tool that we'll use to identify the sentences inside a piece of text
 const winkNLP = require("wink-nlp");
@@ -13,34 +14,32 @@ const nlp = winkNLP(model);
 
 // Require the fastify framework and instantiate it
 const fastify = require("fastify")({
-  // set this to true for detailed logging:
   logger: false,
 });
 
 // Setup our static files
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "../public"),
+  prefix: "/",
 });
 
 // fastify-formbody lets us parse incoming forms
-fastify.register(require("fastify-formbody"));
+fastify.register(require("@fastify/formbody"));
 
 // point-of-view is a templating manager for fastify
-fastify.register(require("point-of-view"), {
+fastify.register(require("@fastify/view"), {
   engine: {
     handlebars: require("handlebars"),
   },
+  root: path.join(__dirname, "../src/pages"),
 });
 
 // Our main GET home page route, pulls from src/pages/index.hbs
 fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
   let params = {
     greeting: "Hello Node!",
   };
-  // request.query.paramName <-- a querystring example
-  reply.view("/src/pages/index.hbs", params);
+  reply.view("index.hbs", params);
 });
 
 // A POST route to handle form submissions
@@ -50,18 +49,7 @@ fastify.post("/", async function (request, reply) {
   let params = {
     results: sendBack,
   };
-  //let testResult = nlp.string.sentences(testText);
-  reply.view("/src/pages/results.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(process.env.PORT, "0.0.0.0", function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
+  reply.view("results.hbs", params);
 });
 
 async function findQuestions(text) {
@@ -85,4 +73,8 @@ async function findQuestions(text) {
   return questionList;
 }
 
-
+// Export the fastify instance for Vercel
+module.exports = async (req, res) => {
+  await fastify.ready();
+  fastify.server.emit('request', req, res);
+};
